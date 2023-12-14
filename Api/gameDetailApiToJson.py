69e53bd7c7b093e87938c8f3440376dde97d2199
ThijnBr <thijnbroekhuizen@hotmail.com>
2023-12-14 15:06:01 +0100

    Try except and currentRow of games

--- Api/gameDetailApiToJson.py
+++ Api/gameDetailApiToJson.py
@@ -2,7 +2,11 @@ import pandas as pd
 import requests
 import time
 
-gameIdLocation = r'C:\Users\jacob\source\repos\ThijnBr\ProjectSteam\Api\gameIds.json'
+import os
+script_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_directory)
+
+gameIdLocation = r'gameIds.json'
 df = pd.read_json(gameIdLocation)
 
 def getGameDetailsApi(gameId):
@@ -12,19 +16,26 @@ def getGameDetailsApi(gameId):
 
 def insertGameDetailsJson(record):
     df = pd.DataFrame(record)
-    return df.to_json(r"C:\Users\jacob\source\repos\ThijnBr\ProjectSteam\Api\gameDetail.json", mode='a', lines=True, orient='records', indent=2, index=False)    
+    return df.to_json(r"gameDetail.json", mode='a', lines=True, orient='records', indent=2, index=False)    
 
 def getGameDetails(df):
     numRows = len(df["applist"]["apps"])
-    for row in range(numRows):
+    currentRow = 41
+    for row in range(currentRow, numRows):
         gameId = df["applist"]["apps"][row]["appid"]
         gameDetail= getGameDetailsApi(gameId)
         print(gameId)
         
-        if str(gameDetail[str(gameId)]) != 'null':
-            if str(gameDetail[str(gameId)]['success']) == 'True' and str(gameDetail[str(gameId)]['data']['type']) == 'game':
-                # print(gameDetail)
-                insertGameDetailsJson(gameDetail)
-                time.sleep(1.5)
+        try:
+            if str(gameDetail[str(gameId)]) != 'null':
+                if str(gameDetail[str(gameId)]['success']) == 'True' and str(gameDetail[str(gameId)]['data']['type']) == 'game':
+                    # print(gameDetail)
+                    insertGameDetailsJson(gameDetail)
+        except:
+            with open ('errorIds.json', 'a')as f:
+                f.write(str(gameId)+',')
+            print('error')
+        currentRow += 1
+        print(currentRow)
     print("It's done!!!")
 getGameDetails(df)
