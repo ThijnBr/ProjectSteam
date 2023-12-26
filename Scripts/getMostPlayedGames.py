7db030b84d704bf474891adbb78914eff9b425d9
GlennJacobs1 <94528688+GlennJacobs1@users.noreply.github.com>
2023-12-26 01:14:08 +0100

    Imported playtime and small changes to formatting of steam friend ids

--- Scripts/getMostPlayedGames.py
+++ Scripts/getMostPlayedGames.py
@@ -1,7 +1,6 @@
-import requests
 import pandas as pd
 import getSteamUserFriends
-
+import getSteamUserGameData
 import os
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -11,6 +10,21 @@ df = pd.read_csv('..\Api\ApiKeys.csv')
 apikey = df.iloc[0, 1]
 
 steamID = 76561198401205997
-friendIds = getSteamUserFriends.getFriendUserIDs(steamID)
-print(friendIds)
+friendIdList = getSteamUserFriends.getFriendUserIDs(steamID)
+
+def getPlayTime():
+    gamesList = []
+    playtimeList = []
+    appidList = []
+    for x in range(2):
+        gamesList.append(getSteamUserGameData.getFriendGames(friendIdList[x]))
+        # print(f"{friendIdList[x]} \n {gamesList[x]}\n")
+        for j in range(gamesList[x]['response']['game_count']-1):    
+            playtimeList.append(f"{gamesList[x]['response']['games'][j]['appid']}, {gamesList[x]['response']['games'][j]['playtime_forever']}")
+        
+    return playtimeList
+def sortPlayeTime():
+    for x in getPlayTime():
+    
+#Bij het opvragen van de games kijk welke game de hoogste playtimeforever en kijk dan of zij in de afgelopen 2 weken die game hebben gespeelt
 
--- Scripts/getSteamUserGameData.py
+++ Scripts/getSteamUserGameData.py
@@ -1,10 +1,15 @@
 import requests
-
 import pandas as pd
+import os
 
-df = pd.read_csv('..\ApiCsv\ApiKeys.csv')
-apikey = df.iloc[0, 1]
+script_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_directory)
 
-steamID = None
 
-apilink = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apikey}&steamid={steamID}&format=json&include_appinfo=true'
+def getFriendGames(steamID):
+    df = pd.read_csv('..\Api\ApiKeys.csv')
+    apikey = df.iloc[0, 1]
+    url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apikey}&steamid={steamID}&format=json&include_appinfo=true'
+    data = requests.get(url)
+    return data.json()
+    
