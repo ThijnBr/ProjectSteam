b0b9ba078b2d44827b42865d7858eae6d9993dd1
GlennJacobs1 <94528688+GlennJacobs1@users.noreply.github.com>
2024-01-18 13:32:20 +0100

    Removal database connection

--- Scripts/sortPlayTime.py
+++ Scripts/sortPlayTime.py
@@ -1,13 +1,40 @@
 import databaseConnection
+import getSteamUserFriends
+import getSteamUserGameData
 
 con = databaseConnection.connect()
 
-def selectPlayTime(con):
-    cursor = con.cursor()
-    cursor.execute("select gamesteam_appid, SUM(playtimelast2weeks), steamusersteamid from playtime group by gamesteam_appid, steamusersteamid")
-    return cursor.fetchall()
+import asyncio
 
-playerTimeList = selectPlayTime(con)
+def getPlayTime(steamID):
+    friendIdList = getSteamUserFriends.getFriendUserIDs(steamID)
+    
+    friend_games_data = asyncio.run(getSteamUserGameData.get_friend_games_async(friendIdList))
+
+    gamesList = []
+
+    for friend_id, data in zip(friendIdList, friend_games_data):
+        if data is not None and 'response' in data and 'games' in data['response']:
+            games = data['response']['games']
+            friend_games = []
+
+            for game in games:
+                try:
+                    gameId = game.get('appid')
+                    playtime_2weeks = int(game.get('playtime_2weeks', 0))
+                    if playtime_2weeks == 0:
+                        continue
+                    friend_games.append([gameId, playtime_2weeks])
+                except (KeyError, ValueError):
+                    continue
+            
+            if friend_games:
+                gamesList.append([friend_id, friend_games])
+    
+    return gamesList
+
+def combineGamePlaytime():
+    
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
