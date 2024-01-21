d32c2064a7774afa00aab3ef5e9ac6600571369b
GlennJacobs1 <94528688+GlennJacobs1@users.noreply.github.com>
2024-01-22 00:52:26 +0100

    finished sortPlayTime

--- Scripts/sortPlayTime.py
+++ Scripts/sortPlayTime.py
@@ -1,3 +1,4 @@
+from multiprocessing.reduction import duplicate
 import databaseConnection
 import getSteamUserFriends
 import getSteamUserGameData
@@ -36,59 +37,37 @@ def getPlayTime(steamID):
 
     return gamesList
 
-def findIndex(stringArr, keyString):
-    result = []
- 
+def findOriginalIndex(stringArr, keyString): 
     for i in range(len(stringArr)):
- 
         for j in range(len(stringArr[i])):
+            # print(stringArr[i][j])
             if stringArr[i][j] == keyString:
-                result.append(i)
-                result.append(j)
-                return result
-    result.append(-1)
-    result.append(-1)
-    
-    return result
+                return i
  
 
 def combineGamePlaytime(steamID):
     playtimeList = getPlayTime(steamID)
+    duplicatesdictSum = {}
 
-    savedGames = []
-    duplicatesTotal = []
     for i in playtimeList:
-        print(i)
-        for j in range(len(i[1])):
-            # print(i[1][j][0])
-            # print(i[1][j][1])
+        for j in range(len(i[1])):             
             gameId = i[1][j][0]
+            playTime = i[1][j][1]
             
-            #Als je een duplicant plaats de gameId in een lijst en in die lijst zet je weer een lijst waarin alle playtimes zitten die bij de gameId horen, bijvoorbeeld: [4000[29, 69]]. 
-            #Maak dan de sum() van duplicateList, bijvoorbeeld: sum(duplicates[i][1])
-            #Vervang de sum met de lijst op positie een
-            #Denk na over wat te doen met originele lijst en de duplicates
+            if gameId in duplicatesdictSum:
+                duplicatesdictSum[gameId] += playTime
+            else:
+                duplicatesdictSum[gameId] = playTime
 
-            #Deze if zoekt of gameId in de 2d lijst zit van savedgames, zoja; slaat op in lijst duplicates
-            # if savedGames and any(gameId in x for x in savedGames):
-            #     #probleem met lijst duplicates, is tijd moet bij elkaar opgeteld 
-            #     print("test")
-            #     indexDupecate = findIndex(savedGames, gameId)
-            #     duplicatesTotal.append([gameId, savedGames[indexDupecate][1]])
-            #     print(duplicatesTotal)
-            # elif gameId not in savedGames:
-            #     savedGames.append([i[1][j][0], i[1][j][1]])
-            #     # print("testElse")
-    
-    return savedGames
+    return [[playTime, duplicatesdictSum[playTime]] for playTime in duplicatesdictSum]
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         j = i
-        while arr[j - 1][1][0] < arr[j][1][0] and j > 0:
+
+        while arr[j - 1][1] < arr[j][1] and j > 0:
             arr[j - 1], arr[j] = arr[j], arr[j - 1]
             j -= 1
     return arr
 
-
-combineGamePlaytime(76561198401205997)
+print(insertion_sort(combineGamePlaytime(76561198401205997)))
